3061a26cb1629a81dd3258b99ca02f3bcd0316e8
eduardoloz <eddieslozano@gmail.com>
2024-03-13 21:48:38 -0400

    added more setup anonymous user stuffs

--- data/setup.sh
+++ data/setup.sh
@@ -11,5 +11,12 @@ sudo apt-get install -y vsftpd
 sudo ufw allow 20/tcp # data channel
 sudo ufw allow 21/tcp # control channel
 
-sudo cp /data/our_vsftpd.conf /etc/vsftpd.conf
 
+# Change the config to allow anonymous access ## note if you ftp you can login to anonymous
+sudo cp /data/our_vsftpd.conf /etc/vsftpd.conf
+sudo systemctl restart vstfpd
+
+# create a directory and test file for anonymous FTP to read
+sudo mkdir -p /vat/ftp/pub
+sudo chown nobody:nogroup /var/ftp/pub
+echo "vsftpd test file " | sudo tee /var/pub/test.txt
